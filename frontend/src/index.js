c7542a43b5da1f2edacaab040a682e1f8cddfa4c
Vikas B Patil <viks.0226@gmail.com>
2023-06-14 18:21:11 +0530

    changes2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const stripePromise=loadStripe('pk_live_51N3dwZSJ2jhyyeEICSKsZuAgmybw4SLuIulruTfptSqBKdEoaSCVsxdbWuKm5ag7TIWl6Og96toeMXXSY6WsbHwH00m232oSVr')
 root.render(
-  
+  <Elements stripe={stripePromise}>
+
     <App />
+  </Elements>
  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
